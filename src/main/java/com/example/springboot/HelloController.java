c27cd598699fcb6654b83ceb0d676d8c93affcaa
ipillai91 <98965503+ipillai91@users.noreply.github.com>
2022-03-04 14:01:46 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! perf1";
+		return "Greetings from Spring Boot + Tanzu! perf2";
 	}
 
 }
