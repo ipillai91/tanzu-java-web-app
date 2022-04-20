7b4b7e34c0ae60f87dab65391504ea9e3ed61ae8
ipillai91 <98965503+ipillai91@users.noreply.github.com>
2022-04-20 16:34:08 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! edit workload April 20";
+		return "Greetings from Spring Boot + Tanzu! edit workload April 20 4pm";
 	}
 
 }
